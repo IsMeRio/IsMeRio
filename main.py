6c9f64a702979269eeb22551399532b0f74222cc
Rio <111352746+IsMeRio@users.noreply.github.com>
2025-04-27 11:15:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -16,4 +16,4 @@ st.markdown(
 st.markdown("<h1 style='text-align: center;'>IsMeRio Main Page</h1>", unsafe_allow_html=True)
 
 st.page_link("pages/bmi.py",label="BMI Calculate")
-st.page_link("pages/command-prompts.py",label="command-prompts-streamlit")
+
